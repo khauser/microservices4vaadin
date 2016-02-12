40850a2c2740212ffa197caeef45e1ea2a4ae798
klhauser <klhauser@epages.de>
2016-02-12 10:08:57 +0100

    * failing TestButton

--- microservices/frontend/src/main/java/microservices4vaadin/ui/MyVaadinUI.java
+++ microservices/frontend/src/main/java/microservices4vaadin/ui/MyVaadinUI.java
@@ -13,6 +13,7 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -29,17 +30,20 @@ public class MyVaadinUI extends UI {
         AcmeUserDetails user = (AcmeUserDetails)sci.getAuthentication().getPrincipal();
 
         VerticalLayout layout = new VerticalLayout();
-        //layout.setSizeFull();
         layout.setWidth("100%");
         layout.setHeight("100%");
 
         Label greetings = new Label(new Label("Hello <b>" + user.getFirstName() + " " + user.getLastName()
-                + "</b>, now you are in the Vaadin UI"));
+                + "</b>, now you are in the Vaadin UI."));
         greetings.setContentMode(ContentMode.HTML);
         greetings.setWidth(null);
 
         layout.addComponent(greetings);
-        layout.setComponentAlignment(greetings, Alignment.MIDDLE_CENTER);
+        layout.setComponentAlignment(greetings, Alignment.BOTTOM_CENTER);
+
+        Button testButton = new Button("TestButton");
+        layout.addComponent(testButton);
+        layout.setComponentAlignment(testButton, Alignment.TOP_CENTER);
 
         setContent(layout);
     }
